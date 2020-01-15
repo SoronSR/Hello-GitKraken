1bffa4e4d3e2f85c3395a2f2a70f49a1e3f5bdf0
Søren Rasmussen <soerensrasmussen@gmail.com>
2020-01-15 23:11:09 +0100

    Change in text

--- Unit1.pas
+++ Unit1.pas
@@ -29,7 +29,7 @@ implementation
 
 procedure TForm1.Button1Click(Sender: TObject);
 begin
-  Label1.Text := 'Hello World!'
+  Label1.Text := 'Hello World! New branch';
 end;
 
 procedure TForm1.Button2Click(Sender: TObject);
